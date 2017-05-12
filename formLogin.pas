c3b6a1b317299b201fb7ba43b67d997fa198658e
leoengcomp <leo.maike@hotmail.com>
2017-05-11 22:33:56 -0300

    Criado formulario de login

--- formLogin.pas
+++ formLogin.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.Menus,
+  Vcl.Imaging.pngimage, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    edtUsuario: TEdit;
+    edtSenha: TEdit;
+    btnEntrar: TBitBtn;
+    btnFechar: TBitBtn;
+    lblUsuario: TLabel;
+    lblSenha: TLabel;
+    imgUser: TImage;
+    procedure btnFecharClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +29,12 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.btnFecharClick(Sender: TObject);
+begin
+  MessageDlg('O sistema será fechado',mtConfirmation,[mbOK],0);
+  application.Terminate;
+
+end;
 
 end.
